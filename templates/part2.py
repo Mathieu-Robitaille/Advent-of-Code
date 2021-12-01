4b27a401231ab4aef350ae6c9c839ba652f3aa30
mrobitaille <mrobitaille@stingray.com>
2021-12-01 15:22:50 -0500

    Updating templates for QOL

--- templates/part2.py
+++ templates/part2.py
@@ -1,3 +1,12 @@
+# Quality of life imports
+from pathlib import Path
+from sys import modules
+
+# Quality of life, define the input file location
+src = Path(modules['__main__'].__file__).resolve().parent
+input_file_path = Path(src, "input.txt")
+
+
 def main():
     pass
 
