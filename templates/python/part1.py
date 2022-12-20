25c441e505bb22ec5df35f2c71b98a3a6311c300
Mathieu Robitaille <mathieujrobitaille@gmail.com>
2022-12-19 19:29:34 -0500

    adding timer to python template

--- templates/python/part1.py
+++ templates/python/part1.py
@@ -1,11 +1,41 @@
 # Quality of life imports
 from pathlib import Path
-from sys import modules
+from sys import modules, path
 
 # Quality of life, define the input file location
 src = Path(modules['__main__'].__file__).resolve().parent
 input_file_path = Path(src, "input.txt")
 
+helper_location = Path(src, "..", "..", "helpers")
+path.insert(1, helper_location.as_posix())
+from timer import time_func
 
-with open(input_file_path) as f:
+EXAMPLE_MODE = True
+example_answer = 0
+example = """"""
+
+@time_func
+def dayXXpart1init(src):
     pass
+
+@time_func
+def dayXXpart1solve():
+    sol = 0
+    print(f"Part 2 answer: {sol}")
+    pass
+
+def part1(src):
+    dayXXpart1init(src)
+    dayXXpart1solve()
+    
+
+if __name__ == "__main__":
+    src = []
+
+    if EXAMPLE_MODE:
+        src = example.split("\n")
+    else:
+        with open(input_file_path) as f:
+            for line in f.readlines():
+                src.append(line.strip('\n'))
+    part1(src)
--- templates/python/part2.py
+++ templates/python/part2.py
@@ -1,10 +1,41 @@
 # Quality of life imports
 from pathlib import Path
-from sys import modules
+from sys import modules, path
 
 # Quality of life, define the input file location
 src = Path(modules['__main__'].__file__).resolve().parent
 input_file_path = Path(src, "input.txt")
 
-with open(input_file_path) as f:
+helper_location = Path(src, "..", "..", "helpers")
+path.insert(1, helper_location.as_posix())
+from timer import time_func
+
+EXAMPLE_MODE = True
+example_answer = 0
+example = """"""
+
+@time_func
+def dayXXpart2init(src):
     pass
+
+@time_func
+def dayXXpart2solve():
+    sol = 0
+    print(f"Part 2 answer: {sol}")
+    pass
+
+def part2(src):
+    dayXXpart2init(src)
+    dayXXpart2solve()
+    
+
+if __name__ == "__main__":
+    src = []
+
+    if EXAMPLE_MODE:
+        src = example.split("\n")
+    else:
+        with open(input_file_path) as f:
+            for line in f.readlines():
+                src.append(line.strip('\n'))
+    part2(src)
